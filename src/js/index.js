5b4bae783f2062e2cdf2f973bab485e1f4681737
nemanjarogic <nemanjarogic93@gmail.com>
2019-07-27 10:08:54 +0200

    Babel configuration

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 // Global app controller
 
 import num from './test';
+const x = 23;
 
-console.log(`I imported ${num} from another module!`);
+console.log(`I imported ${num} from another module! Variable x is ${x}`);
