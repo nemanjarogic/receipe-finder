92f54774c18c6b678f1f23da6e527eb9f8e86525
nemanjarogic <nemanjarogic93@gmail.com>
2019-07-28 09:01:21 +0200

    Rendering AJAX loading spinner

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
  * - Search object
@@ -21,11 +21,13 @@ const controlSearch = async () => {
         // Clear previous search as well as search input
         searchView.clearResults();
         searchView.clearInput();
+        renderLoader(elements.searchRes);
 
         // Search for recepies
         await state.search.getResults();
 
         // Render fetched results on UI
+        clearLoader();
         searchView.renderResults(state.search.result);
     }
 }
